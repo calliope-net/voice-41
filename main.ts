e823aa0e229e9ec28f6f44974046972f4a3d095e
calliope-net <141074597+calliope-net@users.noreply.github.com>
2025-11-14 22:55:04 +0100

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,16 +1,12 @@
+input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
+    pins.play_ode()
+})
 input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
     pins.comment(pins.pins_text("Wachzeit (normal 15 Sekunden)"))
     sekunden = 20
     basic.showNumber(sekunden)
     pins.voice_waketime(sekunden)
 })
-function read_cmdid () {
-    bu = pins.pins_i2cWriteReadBuffer(pins.pins_i2cAdressen(pins.ei2cAdressen.Voive_x64), pins.buffer_fromArray([2]), 1)
-    return pins.buffer_getUint8(bu, 0)
-}
-function musik () {
-    music.startMelody(music.builtInMelody(Melodies.Entertainer), MelodyOptions.Once)
-}
 function feste_Kommandos (id: number) {
     if (id == 45) {
         basic.clearScreen()
@@ -27,9 +23,12 @@ function feste_Kommandos (id: number) {
         basic.clearScreen()
     } else if (id == 69) {
         pins.comment(pins.pins_text("Read temperature"))
+        pins.zeigeText("" + input.temperature() + "°C")
         basic.showNumber(input.temperature())
     } else if (id == 82) {
         control.reset()
+    } else if (id == 92 || id == 102) {
+        pins.play_ode()
     } else if (id == 116) {
         basic.setLedColor(0xff0000)
     } else {
@@ -37,26 +36,28 @@ function feste_Kommandos (id: number) {
     }
 }
 let cmd_id = 0
-let bu: Buffer = null
 let sekunden = 0
 pins.addDisplay(pins.pins_DigitalPin(DigitalPin.C16), pins.pins_DigitalPin(DigitalPin.C17))
 pins.clearDisplays()
-pins.zeigeZahl(10)
+let connected = pins.voice_read_cmdid() >= 0
+if (connected) {
+    basic.setLedColor(0x00ff00)
+    pins.zeigeZahl(1)
+} else {
+    basic.setLedColor(0xff0000)
+    pins.zeigeZahl(-1)
+}
 basic.forever(function () {
-    if (!(basic.simulator())) {
+    if (connected) {
         cmd_id = pins.voice_read_cmdid()
-        if (cmd_id == pins.voice_read_cmdid()) {
-            pins.comment(pins.pins_text("kein Kommando erkannt, 0 nicht anzeigen"))
+        if (cmd_id == 0) {
             basic.pause(2000)
         } else {
-            pins.comment(pins.pins_text("Kommando erkannt, ID anzeigen"))
             pins.zeigeZahl(cmd_id)
-            pins.comment(pins.pins_text("Kommando ID auswerten"))
             if (cmd_id >= 5 && cmd_id <= 21) {
-                pins.comment(pins.pins_text("erlernte Kommandos 5..20 -> A=65..Q=81"))
+                pins.comment(pins.pins_text("erlernte Kommandos 5..20 -> A..Q"))
                 basic.showString(String.fromCharCode(cmd_id + 60))
             } else {
-                pins.comment(pins.pins_text("feste Kommandos 22..141"))
                 feste_Kommandos(cmd_id)
             }
         }
